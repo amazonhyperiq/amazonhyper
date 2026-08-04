2114deba9fa346dd669d358dbb9ab7aba83ec37f
omaraltaee810-code <omaraltaee810@gmail.com>
2026-08-04 14:18:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 let cart = [];
 
 function addToCart(name, price) {
-    alert(name + " تمت إضافته إلى السلة");
+
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    updateCart();
+}
+
+function updateCart() {
+
+    let items = document.getElementById("cart-items");
+    let count = document.getElementById("cart-count");
+    let total = document.getElementById("cart-total");
+
+    items.innerHTML = "";
+
+    let sum = 0;
+
+    cart.forEach(function(item){
+
+        items.innerHTML += `
+        <p>${item.name} - ${item.price.toLocaleString()} د.ع</p>
+        `;
+
+        sum += item.price;
+
+    });
+
+    count.innerHTML = cart.length;
+    total.innerHTML = sum.toLocaleString();
+
+    if(cart.length == 0){
+        items.innerHTML = "لا توجد منتجات في السلة";
+    }
+
+}
+
+function sendToWhatsApp(){
+
+    if(cart.length == 0){
+        alert("السلة فارغة");
+        return;
+    }
+
+    let message = "السلام عليكم، أريد طلب:\n\n";
+
+    cart.forEach(function(item){
+        message += item.name + " - " + item.price.toLocaleString() + " د.ع\n";
+    });
+
+    let total = cart.reduce((a,b)=>a+b.price,0);
+
+    message += "\nالمجموع: " + total.toLocaleString() + " د.ع";
+
+    window.open("https://wa.me/9647700000000?text="+encodeURIComponent(message));
 }
